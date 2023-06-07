734f46b2be52416f764379772f4d7281cb3fc200
Sebastijan Martincic <gluposti112@gmail.com>
2023-06-07 19:59:53 +0200

    add main.py

---
+++ src/main.py
@@ -0,0 +1,25 @@
+import pyglet
+
+
+window = pyglet.window.Window(fullscreen=True)
+
+
+label = pyglet.text.Label('Hello, world',
+                          font_name='Times New Roman',
+                          font_size=36,
+                          x=window.width//2, y=window.height//2,
+                          anchor_x='center', anchor_y='center')
+
+def update(dt):
+    pass
+
+@window.event
+def on_draw():
+    window.clear()
+    label.draw()
+
+
+if __name__ == '__main__':
+    pyglet.clock.schedule_interval(update, 1/60.0)
+    pyglet.app.run()
+
