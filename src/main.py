49d8e8db8ae1ce5591b7e59246f8c975ea42f388
mrsebo67 <sebastijan.x@gmail.com>
2023-06-08 23:59:46 +0200

    mrtvi sugavi pyglet

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ import pyglet
 import scenes.mainScene as mainScene
 
 window = pyglet.window.Window(fullscreen=True)
+fps_display = pyglet.window.FPSDisplay(window=window)
+
 
 
 #test github and vscode on windows
@@ -14,10 +16,13 @@ def on_draw():
     window.clear()
     mainScene.draw()
     label.draw()
+    fps_display.draw()
+
+    
 
 
 if __name__ == '__main__':
-    pyglet.clock.schedule_interval(update, 1/5.0)
+    pyglet.clock.schedule_interval(update, 1/60.0)
 
     mainScene = mainScene.MainScene(window)
     label = pyglet.text.Label("Bok Decki", font_size=35, y = 900)
--- src/scenes/mainScene.py
+++ src/scenes/mainScene.py
@@ -16,11 +16,11 @@ class MainScene:
         self.centerY = window.height//2
 
         self.loadTextures()
+        self.initWorld()
 
         #todo if the world is present in savefiles load the world instead of creating
         self.world = World()
         self.world.createWorld()
-
 
 
     def loadTextures(self):
@@ -31,11 +31,36 @@ class MainScene:
         self.heartSprite.update(scale_y=6)
         self.heartSprite.update(scale_x=8)
 
-        mainSceneBG = pyglet.image.load("sprites/mainSceneBG.png")
+        mainSceneBG = pyglet.image.load("sprites/mainSceneBG2.png")
         mainSceneBG.anchor_x = mainSceneBG.width // 2
         mainSceneBG.anchor_y = mainSceneBG.height // 2
         self.mainSceneBGSprite = pyglet.sprite.Sprite(mainSceneBG, x=self.centerX, y=self.centerY)
-        self.mainSceneBGSprite.update(scale=1.03)
+
+        self.testTile = pyglet.image.load("sprites/waterTile.png")
+        self.testTileSprite = pyglet.sprite.Sprite(self.testTile, x = self.centerX - 416, y=self.centerY - 316)
+        self.testTileSprite2 = pyglet.sprite.Sprite(self.testTile, x=self.centerX + 400, y=self.centerY+300)
+        print((self.centerX+400)-(self.centerX-416))
+        print((self.centerY+300)-(self.centerY-316))
+        
+    def initWorld(self):
+
+        self.sprites = [[0 for x in range(38)] for y in range(51)]
+
+        for i in range(0, 51):
+            for j in range(0, 38):
+                self.sprites[i][j] = pyglet.sprite.Sprite(self.testTile, x = self.centerX - 408 + i*16, y = self.centerY - 308 + j*16)
+
+    def renderWorld(self):
+
+        batch = pyglet.graphics.Batch()
+
+        for elem in self.sprites:
+            for sprite in elem:
+                sprite.batch=batch
+        
+        batch.draw()
+        
+        
 
     def loadWorld(self):
         pass
@@ -46,10 +71,15 @@ class MainScene:
 
     def draw(self):
 
+        pass
         
-        
-        self.heartSprite.draw()
+        #self.heartSprite.draw()
+        self.renderWorld()
         self.mainSceneBGSprite.draw()
+        #self.testTileSprite.draw()
+        #self.testTileSprite2.draw()
+
+        
 
 
     
